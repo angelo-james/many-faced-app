56ab5063f618caa4c2715b4b446cd7ee7b857513
AJ Arriola <angelojames.arriola@gmail.com>
2019-09-23 23:48:20 -0700

    created form component for image link input added some text to show user ranking and used react-particles-js library for a background

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+
+const particlesOptions = {
+  number: {
+    value: 40,
+    density: {
+      enable: true,
+      value_area: 800
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles
+        className='particles'
+        params={{ polygon: {particlesOptions} }} 
+      />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './imageLinkForm.css';
 
 const ImageLinkForm = () => {
   return (
     <div className='ma4 mt0'>
-      
+      <p className='f3 white'>
+        {'Test the power of the Many Faced God to find a face in an image.'}
+      </p>
+      <div className='center'>
+        <div className='form center pa4 br3 shadow-5'>
+          <input className='f4 pa2 w-70 center' type='tex' />
+          <button className='w-30 grow f4 link ph3 pv2 dib white bg-green'>Detect</button>
+        </div>
+      </div>
     </div>
   )
 }
