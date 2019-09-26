57df061adbb240c5165c469e2243512b425f68d1
AJ Arriola <angelojames.arriola@gmail.com>
2019-09-25 18:46:21 -0700

    face recognition component displays image and response from clarifai api responds with data cordinates for image border

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
@@ -22,43 +23,47 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  let state = {
+class App extends Component {
+  state = {
     input: '',
+    imageUrl: '',
   }
   
-  const onInputChange = (e) => {
-    console.log(e.target.value);
+  onInputChange = (e) => {
+    this.setState({ input: e.target.value });
   }
 
-  const onSubmit = () => {
-    console.log('you clicked me');
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+  onSubmit = () => {
+    this.setState({ imageUrl: this.state.input })
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
     function(response) {
-      console.log(response);
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
     },
     function(err) {
       // there was an error
     }
   );
   }
-  
-  return (
-    <div className="App">
-      <Particles
-        className='particles'
-        params={{ polygon: {particlesOptions} }} 
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm 
-        onInputChange={ onInputChange }
-        onSubmit={ onSubmit }
-      />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Particles
+          className='particles'
+          params={{ polygon: {particlesOptions} }} 
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={ this.onInputChange }
+          onSubmit={ this.onSubmit }
+        />
+        <FaceRecognition 
+          imageUrl={this.state.imageUrl}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
